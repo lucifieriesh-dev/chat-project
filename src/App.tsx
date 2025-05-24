3431ffb9637d8d9aedd2b408781b9721da845c15
lucifieriesh-dev <hit704256@gmail.com>
2025-05-24 12:54:13 +0530

    Implement Dropdown Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,30 @@ import ChatArea from './components/chat/ChatArea';
 import AICopilot from './components/ai/AICopilot';
 import Footer from './components/Footer';
 import useChatStore from './store/chatStore';
+import { Menu } from '@headlessui/react';
 
 function App() {
   const { isAICopilotVisible } = useChatStore();
   
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex-1 bg-primary-100/30 flex items-center justify-center p-4">
-        <div className="bg-white rounded-lg shadow-xl flex h-[80vh] w-full max-w-7xl overflow-hidden">
-          <div className="w-72 flex-shrink-0">
-            <ConversationList />
+    <Menu as="div">
+      <div className="min-h-screen flex flex-col">
+        <div className="flex-1 bg-primary-100/30 flex items-center justify-center p-4">
+          <div className="bg-white rounded-lg shadow-xl flex h-[80vh] w-full max-w-7xl overflow-hidden">
+            <div className="w-72 flex-shrink-0">
+              <ConversationList />
+            </div>
+            
+            <ChatArea />
+            
+            {isAICopilotVisible && (
+              <AICopilot />
+            )}
           </div>
-          
-          <ChatArea />
-          
-          {isAICopilotVisible && (
-            <AICopilot />
-          )}
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Menu>
   );
 }
 
